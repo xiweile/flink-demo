4113bef8f41df78cda80179b9de902f7a881bd69
xiweile <weiller@126.com>
2021-03-31 11:46:21 +0800

    提交

---
+++ src/main/java/com/weiller/flinkdemo/spendreport/FraudDetector.java
@@ -0,0 +1,77 @@
+package com.weiller.flinkdemo.spendreport;
+
+import jdk.nashorn.internal.codegen.types.Type;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+
+/**
+ * 账号金额欺诈探测器
+ */
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final double SMALL_AMOUNT = 1.00;
+    private static final double LARGE_AMOUNT = 500.00;
+    private static final long ONE_MINUTE = 60 * 1000;
+
+    // 欺诈条件标志
+    private transient ValueState<Boolean> flagState;
+
+    // flag标志重置计时器时间
+    private transient ValueState<Long> timerState;
+
+    @Override
+    public void open(Configuration parameters){
+        ValueStateDescriptor<Boolean> flagDescriptor = new ValueStateDescriptor<>("flag", Types.BOOLEAN);
+        flagState =  getRuntimeContext().getState(flagDescriptor);
+
+        ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>("timer-state", Types.LONG);
+        timerState  = getRuntimeContext().getState(timerDescriptor);
+    }
+
+    @Override
+    public void processElement(Transaction transaction, Context context, Collector<Alert> collector) throws Exception {
+        Boolean lastTransactionWasSmall  = flagState.value();
+        if (lastTransactionWasSmall !=null){
+            if (transaction.getAmount()>LARGE_AMOUNT){
+                Alert alert = new Alert();
+                alert.setId(transaction.getAccountId());
+                collector.collect(alert);
+            }
+
+            cleanUp(context);
+        }
+
+        if (transaction.getAmount() < SMALL_AMOUNT){
+            flagState.update(true);
+
+            long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+            context.timerService().registerProcessingTimeTimer(timer);
+            timerState.update(timer);
+        }
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+        // remove flag after 1 minute
+        timerState.clear();
+        flagState.clear();
+    }
+
+    private void cleanUp(Context ctx) throws Exception {
+        // delete timer
+        Long timer = timerState.value();
+        ctx.timerService().deleteProcessingTimeTimer(timer);
+
+        // clean up all state
+        timerState.clear();
+        flagState.clear();
+    }
+}
